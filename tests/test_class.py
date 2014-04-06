fd04434719c73ec34777bdc2896353650044d043
Carlos Llamacho <llamacholancho@gmail.com>
2014-04-06 14:05:55 -0400

    Agregamos dos pruebas que fallan con nuestro modelo, por no tener campo max_students y no tener metodo validate.

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,13 +1,14 @@
 #-*- coding:utf-8-*-
 from openerp.tests import common
 from faker import Faker
+from openerp.osv import orm
 
 
 class TestCourse(common.TransactionCase):
 
-    def create_course(self, cr, uid, model_obj):
+    def create_course(self, cr, uid, model_obj, max_number):
         """Metodo ayudante para crear cursos."""
-        values = {'name': "Test Course", 'subject': 'Python'}
+        values = {'name': "Test Course", 'subject': 'Python', 'max_students': max_number}
         return model_obj.create(cr, uid, values)
 
     def create_students(self, cr, uid, model_obj, amount, course_id):
@@ -33,9 +34,13 @@ class TestCourse(common.TransactionCase):
         self.student_obj = self.registry('student.student')
 
         #Crear la data de prueba, SI quiero usar ORM para esto.
-        self.course_id = self.create_course(cr, uid, self.course_obj)
-        self.student_ids = self.create_students(cr, uid, self.student_obj, 3,
-                                                self.course_id)
+        #Esta sera la data para mis pruebas positivas.
+        self.course_id = self.create_course(cr, uid, self.course_obj, 5)
+        self.create_students(cr, uid, self.student_obj, 3, self.course_id)
+
+        #Esta sera la data para mis pruebas negativas
+        self.bad_course_id = self.create_course(cr, uid, self.course_obj, 3)
+        self.create_students(cr, uid, self.student_obj, 5, self.bad_course_id)
 
 
     #Vamos a crear una prueba muy sencilla, solo para que falle.
@@ -47,8 +52,22 @@ class TestCourse(common.TransactionCase):
 
 
     def testRelacionarEstudiantesPorCurso(self):
-        """Checqueamos que desde nuestro curso podemos llevar un control de
+        """Chequeamos que desde nuestro curso podemos llevar un control de
         estudiantes registrados."""
         cr, uid = self.cr, self.uid
         curso = self.course_obj.browse(cr, uid, self.course_id)
         self.assertTrue(curso.registered_students)
+
+    def testCourseMaxStudents(self):
+        """Chequeamos que nuestro curso tenga un numero maximo de estudaintes."""
+        cr, uid = self.cr, self.uid
+        curso = self.course_obj.browse(cr, uid, self.course_id)
+        self.assertTrue(curso.max_students)
+
+    def testRaiseExceptionAtNumberExceed(self):
+        """Chequamos que levante una excepcion al infringir numero maximo."""
+        cr, uid = self.cr, self.uid
+        #Aqui no quiero usar mi data de curso valido, pues tendria que cambiarla
+        #para solamente esta prueba. Es posible, pero es mejor crear otro curso
+        #para que falle.
+        self.assertRaises(orm.except_orm, self.course_obj.validate, cr, uid, self.bad_course_id)
